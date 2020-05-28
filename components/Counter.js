ed0b18c22b0192af692699849ed35eaddb935590
Kate Sowles <hello@katesowles.com>
2020-05-28 09:11:06 -0700

    lesson one complete with notes inline

--- components/Counter.js
+++ components/Counter.js
@@ -5,7 +5,33 @@ const Button = ({ children, ...props }) => {
 }
 
 export default function Counter() {
-  let count = 0
+  // NOTE: useState returns a tuple:
+  // let count = useState(0);
+  // let variable = count[0];
+  // let setter = count[1];
+  // but most folks just destructure it as shown below
+  let [count, setCount] = useState(0)
+  let [error, setError] = useState(null)
+  // NOTE: use state's "set" function is very performant, it creates a diff and only calls setVariable when the value to be set is different form the current state value.
+  // NOTE: useState is local to the component, so the state can't "escape" from each instance of a component... So if we used more than one <Counter /> in a parent component, they'll each have their own state to be managed separately.
+  // NOTE: someone in the workshop asked, how many useStates in a component before you switch to a useReducer — Cassidy's response was that her "number" is usually about 4 useStates, but we'll get into that later.
 
-  return <div>Let's count: {count} </div>
+  const add = () => {
+    setCount(count + 1)
+    setError(null)
+  }
+
+  const subtract = () => {
+    if (count > 1) setCount(count - 1)
+    else setError('value should be positive')
+  }
+
+  return (
+    <>
+      <Button onClick={subtract}>Subtract</Button>
+      <div>Let's count: {count} </div>
+      <Button onClick={add}>Add</Button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </>
+  )
 }
--- pages/lessons/1-props-and-state.js
+++ pages/lessons/1-props-and-state.js
@@ -1,5 +1,47 @@
-// import Counter from '@components/Counter'
+import React from 'react'
+// NOTE: the @components rule is set in the config, it's basically a shortcut to the components folder! Could be SUPER useful in larger, more nested applications
+import Counter from '@components/Counter'
+// NOTE: declarative (React, state-driven) : WHAT you want in an app state; OR setting a thermostat to the specific temp you want to have, and we "don't care how you do it", just make it happen.
+// NOTE: imperative (gross jQuery example of parsing text to int, then incrementing value by n) : HOW you get to an app state; OR if too hot, turn the fan speed up, otherwise turn the fan speed down.
+
+const reactElement = <div>Henlo</div>
+const reactComponent = () => reactElement
+// NOTE: the difference between React Element and React Component is that a React Component is a function that returns a React Element
+
+// NOTE: reactEl is the same as reactElement above — first param is tag type second param is props/attributes, third param is content
+// const reactEl = React.createElement('div', null, 'Henlo')
+
+const Button = ({ children, icon, handleClick }) => {
+  // NOTE: the children prop is often confused BECAUSE if it is set to a single element, it's an object; otherwise its an array of objects
+
+  // NOTE: `<Button>{element}{element2}</Button>` is the same as `React.createElement(Button, { children: [ element, element2 ]});`
+
+  return (
+    <button onClick={handleClick}>
+      {icon} {children}
+    </button>
+  )
+}
 
 export default function PropsState() {
-  return null
+  return <Counter />
+
+  // return (
+  //   <>
+  //     <Button
+  //       icon="💚"
+  //       handleClick={() => {
+  //         console.log('button was clicked!')
+  //       }}
+  //     >
+  //       Children
+  //     </Button>
+  //     <Button icon="😂">Second Button Children</Button>
+  //   </>
+  // )
+
+  // return <div>Henlo</div>
+  // NOTE: the return statement below with the element abstracted out is the same as the return above
+  // return reactElement
+  // NOTE: the return statement below is the same as the return statement with the div as it returns the reusable component that returns the original reactElement
 }
