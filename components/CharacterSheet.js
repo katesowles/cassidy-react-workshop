bc10cb9ea4706c1c375e27df112ed10d7ec576ca
Kate Sowles <hello@katesowles.com>
2020-05-28 12:17:14 -0700

    lesson five complete with notes inline

--- components/CharacterSheet.js
+++ components/CharacterSheet.js
@@ -1,5 +1,47 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import friendlyWords from 'friendly-words'
+
+// NOTE: Cassidy linked this gist showing how to combine useReducer and useEffect to create a custom usePromise hook: https://gist.github.com/cassidoo/ecaac347694aeb6f906464fbc8ef68c7
+
+// NOTE: useState is just useReducer under the hood:
+// const useMyState = (initialValue) {
+//   let reducer = (state, action) => state;
+//   let [state, dispatch] = useReducer(reducer, initialValue)
+//   return [state, dispatch];
+// }
+
+// NOTE: First Example using the .reduce method with a numeric state value
+// let array = [1, 2, 3, 4, 5]
+// let add = (x, y) => x + y
+// let sum = array.reduce(add, 0)
+// 0 + 1
+// 1 + 2
+// 3 + 3
+// 6 + 4
+// 10 + 5
+// sum => 15
+// console.log({ sum })
+
+// NOTE: Second Example using the .reduce method with a state object
+// let initialState = { count: 0, cake: true }
+// let actions = [
+//   { type: 'ADD', by: 2 },
+//   { type: 'MINUS', by: 4 },
+//   { type: 'EAT_CAKE' }
+// ]
+// const reducer = (state, action) => {
+//   // return state + action
+//   if (action.type === 'ADD') {
+//     return { ...state, count: state.count + action.by }
+//   } else if (action.type === 'MINUS') {
+//     return { ...state, count: state.count - action.by }
+//   } else if (action.type === 'EAT_CAKE') {
+//     return { ...state, cake: false }
+//   } else {
+//     return state
+//   }
+// }
+// console.log(actions.reduce(reducer, initialState))
 
 let backgrounds = [
   'Noble',
@@ -9,7 +51,7 @@ let backgrounds = [
   'Criminal',
   'Hermit',
   'Guild Artisan',
-  'Sage',
+  'Sage'
 ]
 
 function randomBackground() {
@@ -23,54 +65,106 @@ function randomName() {
 }
 
 export default function App() {
-  let [darkMode, setDarkMode] = useState(false)
-  let [name, setName] = useState('')
-  let [background, setBackground] = useState('')
-  let [error, setError] = useState(null)
+  let reducer = (state, action) => {
+    switch (action.type) {
+      case 'BG_SELECTED': {
+        return { ...state, background: action.value }
+      }
+      case 'BG_NOT_EXIST': {
+        return { ...state, error: 'Background does not exist' }
+      }
+      case 'NO_ERRORS': {
+        return { ...state, error: null }
+      }
+      case 'TOGGLE_DARKMODE': {
+        return { ...state, darkMode: !state.darkMode }
+      }
+      case 'LONG_NAME': {
+        return { ...state, error: 'Name is too long' }
+      }
+      case 'SET_NAME': {
+        return { ...state, name: action.name }
+      }
+      case 'RANDOMIZE': {
+        return { ...state, name: randomName(), background: randomBackground() }
+      }
+    }
+  }
+
+  let initialState = {
+    darkMode: false,
+    name: '',
+    background: '',
+    error: null
+  }
+
+  let [state, dispatch] = useReducer(reducer, initialState)
+
+  // NOTE: our reducer function (above) allows us to replace all the useStates below with the useReducer above
+  // let [darkMode, setDarkMode] = useState(false)
+  // let [name, setName] = useState('')
+  // let [background, setBackground] = useState('')
+  // let [error, setError] = useState(null)
 
   function handleBackgroundSelect(event) {
     let value = event.target.value
-    setBackground(value)
+    dispatch({ type: 'BG_SELECTED' })
+    // dispatch replaces setBackground useState below
+    // setBackground(value)
+
     if (!backgrounds.includes(value)) {
-      setError('This background does NOT exist.')
+      dispatch({ type: 'BG_NOT_EXIST' })
+      // dispatch replaces setError useState below
+      // setError('This background does NOT exist.')
     } else {
-      setError(null)
+      dispatch({ type: 'NO_ERRORS' })
+      // dispatch replaces setError useState below
+      // setError(null)
     }
   }
 
   return (
     <>
-      <div className={`App ${darkMode ? 'darkmode' : ''}`}>
+      <div className={`App ${state.darkMode ? 'darkmode' : ''}`}>
         <button
           onClick={() => {
-            setDarkMode(!darkMode)
+            dispatch({ type: 'TOGGLE_DARKMODE ' })
+            // dispatch replaces setDarkMode useState below
+            // setDarkMode(!darkMode)
           }}
         >
-          Dark Mode {darkMode ? 'ON' : 'OFF'}
+          Dark Mode {state.darkMode ? 'ON' : 'OFF'}
         </button>{' '}
         <br />
         <input
           type="text"
           placeholder="Type your name"
-          value={name}
-          onChange={(event) => {
-            setName(event.target.value)
+          value={state.name}
+          onChange={event => {
+            dispatch({ type: 'SET_NAME ', name: event.target.value })
+            // dispatch replaces setName useState below
+            // setName(event.target.value)
+
             if (event.target.value.length > 15) {
-              setError('Name is WAY too long, bucko.')
+              dispatch({ type: 'LONG_NAME' })
+              // dispatch replaces setError useState below
+              // setError('Name is WAY too long, bucko.')
             }
           }}
         />
-        <select value={background} onChange={handleBackgroundSelect}>
-          {backgrounds.map((b) => {
+        <select value={state.background} onChange={handleBackgroundSelect}>
+          {backgrounds.map(b => {
             return <option key={`bg-${b}`}>{b}</option>
           })}
         </select>
-        {error && (
+        {state.error && (
           <div className="error">
-            {error}
+            {state.error}
             <button
               onClick={() => {
-                setError(null)
+                dispatch({ type: 'NO_ERRORS' })
+                // dispatch replaces setError useState below
+                // setError(null)
               }}
             >
               Dismiss
@@ -78,13 +172,15 @@ export default function App() {
           </div>
         )}
         <div className="sheet">
-          <h2>Name: {name}</h2>
-          <h2>Background: {background}</h2>
+          <h2>Name: {state.name}</h2>
+          <h2>Background: {state.background}</h2>
         </div>
         <button
           onClick={() => {
-            setName(randomName())
-            setBackground(randomBackground())
+            dispatch({ type: 'RANDOMIZE' })
+            // dispatch replaces setName AND setBackground useState below
+            // setName(randomName())
+            // setBackground(randomBackground())
           }}
         >
           Do it all for me instead
